bea9ab36133381b7930e5db6f4174976735d2a60
stanlypalathingal <b9058657@newcastle.ac.uk>
2019-11-12 17:10:52 +0000

    started the second file to deal wtih the question response

--- munge/01-A.R
+++ munge/01-A.R
@@ -1,1 +1,3 @@
 # Example preprocessing script.
+#setwd("C:/Users/HP/OneDrive - Newcastle University/Data management and exploratory Data Analysis/EDA_Project/")
+setwd("C:/Users/b9058657/OneDrive - Newcastle University/Data management and exploratory Data Analysis/EDA_Project")
