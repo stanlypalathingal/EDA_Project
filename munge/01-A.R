70789023250304cb51991861922edaff494ea788
[b9058657@newcastle.ac.uk] <b9058657@newcastle.ac.uk>
2019-11-13 18:10:35 +0000

    Working on the question response with good progress

--- munge/01-A.R
+++ munge/01-A.R
@@ -1,3 +1,4 @@
 # Example preprocessing script.
-#setwd("C:/Users/HP/OneDrive - Newcastle University/Data management and exploratory Data Analysis/EDA_Project/")
-setwd("C:/Users/b9058657/OneDrive - Newcastle University/Data management and exploratory Data Analysis/EDA_Project")
+setwd("C:/Users/HP/OneDrive - Newcastle University/Data management and exploratory Data Analysis/EDA_Project/")
+#setwd("C:/Users/b9058657/OneDrive - Newcastle University/Data management and exploratory Data Analysis/EDA_Project")
+
